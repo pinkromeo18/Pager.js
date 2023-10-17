42a7cdaf06e9879f847dd5a483bb792c09122331
ピンクロメオ <99705203+pinkromeo18@users.noreply.github.com>
2023-10-17 12:07:22 +0900

    Update Pager.js

--- Pager.js
+++ Pager.js
@@ -1,78 +1,40 @@
 /*v1.0 need Pager.css*/
-/*import {Pager} from "..../Pager.js" */
-
-/*usage 
-var pager=Pager();
+/*
+//@import url(https://pinkromeo18.github.io/Pager.js/Pager.css);
+import {Pager} from "https://pinkromeo18.github.io/Pager.js/Pager.js"
+//pager sample
+var pager=Pager("app")
 pager.add({
-  html:"<p></p><p>xxx</p>",
-  css:`background-image: url(https://mae.chab.in/wp-content/uploads/2015/09/QsjPn.jpg);`
+  id:"yyy",
+  innerHTML:"xxxya",
+  style:"background:#234; color:ghostwhite;"
 }).add({
-  html:"<input type='text'></input><button>呼び出し</button>",
-  css:"background-color: #ccc;"
-}).add({
-  html:"３番",
-  css:"background-color: #333;"
-});
-var page2=pager.page(2);
-
-
-{
-  var page1 =pager.page(1).querySelector('p')
-  page1.textContent="zzzz"
-  var count = 1
-  var po1 = Polling(()=>{
-    page1.textContent= count++
-  },1000)
-  po1.start()
-}
+  id:"zzz",
+  innerHTML:"xxxya",
+  style:"background:orange; color:black;"
+})
+pager.page(2).textContent="this is page 2"
+pager.q("#yyy").textContent="id #yyy"
+console.log(pager)
 */
 
 export function Pager(id){
-
-  const topclass ="pages",pageclass="page"
+  const topclass="pages",pageclass="page"
   var o={}
-    o.topdom = isObject(id)?id:document.getElementById(id)
-    if(isNullable(o.topdom)){
-      o.topdom =document.body
-      console.error("Pager: id notfound. set the body")
-    }  
-    o.topdom.classList.add(topclass)
-    o.totalPage=0
-    o.topclass =topclass
-    o.pageclass=pageclass
-    ;
-    o.add =(_html,css)=>{
-      if(isObject(_html)){
-        let {html,css}=_html;
-        return o.add(html,css)
-      }
-      o.totalPage++;
-      var el = createPage(o.totalPage);
-      el.innerHTML = _html||o.totalPage
-      el.setAttribute("style",css||"");
-      o.topdom.appendChild(el)
-      return o
-    }
-    o.page=(number)=>{
-      var q = `.${pageclass}-${number}`
-      return document.querySelector(q)
-    }
-    
-    return Object.assign({},o)
+  o.dom=document.getElementById(id)||document.body
+  o.dom.classList.add(topclass)
+  //
+  o.add=(obj)=>{
+    var el = Object.assign(document.createElement('div'),obj)    
+    el.classList.add(pageclass)
+    o.dom.appendChild(el)
+    return o
+  }
+  o.page=(num)=>{
+    var el= o.dom.querySelector(`.${pageclass}:nth-child(${num})`)
+    return el
+  }
+  o.q=(query)=>o.dom.querySelector(query)
   ;
-  
-  function isNullable(d){
-    return d===null || d=== undefined
-  }
-  function isObject(d) {
-    return d !== null && typeof d === 'object'
-  }
-  function createPage(number){
-    var d =document.createElement("div")
-    d.classList.add(pageclass)
-    var c = `${pageclass}-${number}`
-    d.classList.add(c)
-    return d    
-  }
+  return Object.assign({},o,{topclass,pageclass})
 }
-
